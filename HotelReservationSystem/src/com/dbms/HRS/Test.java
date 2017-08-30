341f4df885c19c3d4b8feb390874e681e8ac6db6
dharshini18 <govindarajan.d@husky.neu.edu>
2017-08-30 15:22:39 -0400

    Commit1 - Inital Commit

---
+++ HotelReservationSystem/src/com/dbms/HRS/Test.java
@@ -0,0 +1,13 @@
+package com.dbms.HRS;
+
+public class Test {
+	public static void main(String args[]){
+		int arr[] = {2, 3, 4, 10, 40};
+        int l = 0, r = arr.length - 1;
+        int m = l + (r-l)/2;
+		int a = l + (r-1)/2;
+		System.out.println(a);
+		System.out.println(m);
+	}
+
+}
